8dc090c1df8afb429a756dfd82c8532b95d51ea8
flamingmustache <yivasyk@yahoo.com>
2016-04-29 13:23:33 -0700

    tes

---
+++ Assets/Scripts/Spawn.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawn : MonoBehaviour {
+	private GameObject player;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnTriggerExit (Collider other){
+		transform.position = new Vector3(-3.16f, 0.54f, 5.15f);
+	}
+}
